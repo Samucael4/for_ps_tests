f9ed50040bf9e6bae20711dd37d2fc2de2e298e8
Samuel Marques de Oliveira <99765333+Samucael4@users.noreply.github.com>
2025-09-22 20:15:42 +0100

    Add files via upload

---
+++ tester.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+test_list() {
+	nums="$1"
+	moves=$(./push_swap $nums | wc -l)
+	checker=$(./push_swap $nums | ./checker $nums)
+	echo "numeros: $nums || $moves movimentos || checker: $checker"
+}
+
+test_list_sem_numeros() {
+	nums="$1"
+	#moves=$(./push_swap $nums | ./checker $nums)
+	moves=$(./push_swap $nums | wc -l)
+	checker=$(./push_swap $nums | ./checker $nums)
+	echo "$moves movimentos || checker: $checker"
+}
+
+test_invalid() {
+	input="$1"
+	output=$(./push_swap "$input" 2>&1)
+	echo "$output"
+}
+
+# test_list_valgrind() {
+#     nums="$1"
+# 	moves=$(valgrind --leak-check=full --show-leak-kinds=all ./push_swap $nums | wc -l)
+# 	checker=$(valgrind --leak-check=full --show-leak-kinds=all ./push_swap $nums | ./checker $nums)
+#     echo "$moves movimentos checker: $checker"
+# }
+
+# test_list_sem_numeros_valgrind() {
+#     nums="$1"
+# 	moves=$(valgrind --leak-check=full --show-leak-kinds=all ./push_swap $nums | wc -l)
+# 	checker=$(valgrind --leak-check=full --show-leak-kinds=all ./push_swap $nums | ./checker $nums)
+#     echo "$moves movimentos checker: $checker"
+# }
+
+echo "Testando 3 números"
+for a in 1 2 3; do
+  for b in 1 2 3; do
+	for c in 1 2 3; do
+	  if [ "$a" != "$b" ] && [ "$a" != "$c" ] && [ "$b" != "$c" ]; then
+		test_list "$a $b $c"
+	  fi
+	done
+  done
+done
+
+echo "Testando 5 números"
+for a in 1 2 3 4 5; do
+  for b in 1 2 3 4 5; do
+    for c in 1 2 3 4 5; do
+      for d in 1 2 3 4 5; do
+        for e in 1 2 3 4 5; do
+          if [ "$a" != "$b" ] && [ "$a" != "$c" ] && [ "$a" != "$d" ] && [ "$a" != "$e" ] &&
+             [ "$b" != "$c" ] && [ "$b" != "$d" ] && [ "$b" != "$e" ] &&
+             [ "$c" != "$d" ] && [ "$c" != "$e" ] &&
+             [ "$d" != "$e" ]; then
+            test_list "$a $b $c $d $e"
+          fi
+        done
+      done
+    done
+  done
+done
+
+
+echo "Testando 100 números"
+for i in {1..10}; do
+	nums=$(shuf -i 1-100 -n 100)
+	test_list_sem_numeros "$nums"
+done
+
+echo "Testando 500 números"
+for i in {1..10}; do
+	nums=$(shuf -i 1-500 -n 500)
+	test_list_sem_numeros "$nums"
+done
+
+echo "Testando entradas inválidas"
+test_invalid ""
+test_invalid "   "
+test_invalid "--1 5 4 2"
+test_invalid "++5 8 2"
+test_invalid "42ggh 4 1"
+test_invalid "6859g 229 793"
+test_invalid "1 -1 2 -2 2147483648"
+test_invalid "1 -1 2 -2 -2147483649"
+test_invalid "2 1 3 -2 1"
+
+echo "Entradas invlaidas sem mensagem de erro"
+test_invalid "-1 0 1 2 3 4 5 6 7 8 9"
+test_invalid "42"
+./push_swap
+
